031040ddc2d6f031622bc6bfd90fbc8dbf20b735
AmandaMedina17 <155869016+AmandaMedina17@users.noreply.github.com>
2026-06-03 12:43:29 -0400

    feat: frontend port configurable from .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const port = process.env.FRONTEND_PORT ? parseInt(process.env.FRONTEND_PORT) : 3000;
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: port,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
